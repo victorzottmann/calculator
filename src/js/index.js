3d600120cea9526ac0e3e11f8a1df79de1501141
Victor Zottmann <zottmann.victor@gmail.com>
2024-06-12 19:05:58 +1000

    style: add calculator container

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 const root = document.querySelector("#root");
-const h1 = document.createElement("h1")
-h1.textContent = "Hello World";
-root.appendChild(h1);
+
+const calculator = document.createElement("div");
+calculator.classList.add("calculator");
+root.appendChild(calculator);
