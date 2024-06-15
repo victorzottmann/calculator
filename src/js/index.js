ab0c29ab018d410f46e829daa8c3df4eda6d4b25
Victor Zottmann <zottmann.victor@gmail.com>
2024-06-15 22:00:31 +1000

    style: add and position all buttons

--- src/js/index.js
+++ src/js/index.js
@@ -16,3 +16,138 @@ const displayText = document.createElement("span");
 displayText.classList.add("content__display__text");
 displayText.textContent = "100,000,000";
 display.appendChild(displayText);
+
+const buttons = document.createElement("div");
+buttons.classList.add("content__buttons");
+content.appendChild(buttons);
+
+
+// FIRST ROW
+const firstRow = document.createElement("div");
+firstRow.classList.add("content__buttons__row");
+
+const acBtn = document.createElement("button");
+acBtn.classList.add("btn");
+acBtn.textContent = "AC";
+firstRow.appendChild(acBtn);
+
+const plusMinusBtn = document.createElement("button");
+plusMinusBtn.classList.add("btn");
+plusMinusBtn.textContent = "+/-"
+firstRow.appendChild(plusMinusBtn);
+
+const percentBtn = document.createElement("button");
+percentBtn.classList.add("btn");
+percentBtn.textContent = "%";
+firstRow.appendChild(percentBtn);
+
+const divideBtn = document.createElement("button");
+divideBtn.classList.add("btn");
+divideBtn.textContent = "/";
+firstRow.appendChild(divideBtn);
+
+
+// SECOND ROW
+const secondRow = document.createElement("div");
+secondRow.classList.add("content__buttons__row");
+buttons.appendChild(secondRow);
+
+const sevenBtn = document.createElement("button");
+sevenBtn.classList.add("btn");
+sevenBtn.textContent = "7";
+secondRow.appendChild(sevenBtn);
+
+const eightBtn = document.createElement("button");
+eightBtn.classList.add("btn");
+eightBtn.textContent = "8";
+secondRow.appendChild(eightBtn);
+
+const nineBtn = document.createElement("button");
+nineBtn.classList.add("btn");
+nineBtn.textContent = "9";
+secondRow.appendChild(nineBtn);
+
+const timesBtn = document.createElement("button");
+timesBtn.classList.add("btn");
+timesBtn.textContent = "X";
+secondRow.appendChild(timesBtn);
+
+// THIRD ROW
+const thirdRow = document.createElement("div");
+thirdRow.classList.add("content__buttons__row");
+buttons.appendChild(thirdRow);
+
+const fourBtn = document.createElement("button");
+fourBtn.classList.add("btn");
+fourBtn.textContent = "4";
+thirdRow.appendChild(fourBtn);
+
+const fiveBtn = document.createElement("button");
+fiveBtn.classList.add("btn");
+fiveBtn.textContent = "5";
+thirdRow.appendChild(fiveBtn);
+
+const sixBtn = document.createElement("button");
+sixBtn.classList.add("btn");
+sixBtn.textContent = "6";
+thirdRow.appendChild(sixBtn);
+
+const minusBtn = document.createElement("button");
+minusBtn.classList.add("btn");
+minusBtn.textContent = "-";
+thirdRow.appendChild(minusBtn);
+
+
+// FOURTH ROW
+const fourthRow = document.createElement("div");
+fourthRow.classList.add("content__buttons__row");
+buttons.appendChild(fourthRow);
+
+const oneBtn = document.createElement("button");
+oneBtn.classList.add("btn");
+oneBtn.textContent = "1";
+fourthRow.appendChild(oneBtn);
+
+const twoBtn = document.createElement("button");
+twoBtn.classList.add("btn");
+twoBtn.textContent = "2";
+fourthRow.appendChild(twoBtn);
+
+const threeBtn = document.createElement("button");
+threeBtn.classList.add("btn");
+threeBtn.textContent = "3";
+fourthRow.appendChild(threeBtn);
+
+const plusBtn = document.createElement("button");
+plusBtn.classList.add("btn");
+plusBtn.textContent = "+";
+fourthRow.appendChild(plusBtn);
+
+
+// FITH ROW
+const fifthRow = document.createElement("div");
+fifthRow.classList.add("content__buttons__row");
+buttons.appendChild(fifthRow);
+
+const zeroBtn = document.createElement("button");
+zeroBtn.classList.add("btn");
+zeroBtn.textContent = "0";
+fifthRow.appendChild(zeroBtn);
+
+const dotBtn = document.createElement("button");
+dotBtn.classList.add("btn");
+dotBtn.textContent = ".";
+fifthRow.appendChild(dotBtn);
+
+const equalsBtn = document.createElement("button");
+equalsBtn.classList.add("btn");
+equalsBtn.classList.add("btn-equals");
+equalsBtn.textContent = "=";
+fifthRow.appendChild(equalsBtn);
+
+
+buttons.appendChild(firstRow);
+buttons.appendChild(secondRow);
+buttons.appendChild(thirdRow);
+buttons.appendChild(fourthRow);
+buttons.appendChild(fifthRow);
