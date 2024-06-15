1e4a304dc268f5ce84f7367f6420ad14cdd3db05
Victor Zottmann <zottmann.victor@gmail.com>
2024-06-15 20:49:03 +1000

    feat: add display and update color scheme

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,16 @@ const root = document.querySelector("#root");
 const calculator = document.createElement("div");
 calculator.classList.add("calculator");
 root.appendChild(calculator);
+
+const content = document.createElement("div");
+content.classList.add("content");
+calculator.appendChild(content);
+
+const display = document.createElement("div");
+display.classList.add("content__display");
+content.appendChild(display);
+
+const displayText = document.createElement("span");
+displayText.classList.add("content__display__text");
+displayText.textContent = "100,000,000";
+display.appendChild(displayText);
