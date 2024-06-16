433ec62668a97b976540bd93f1a8172f700970c1
Victor Zottmann <zottmann.victor@gmail.com>
2024-06-16 21:49:25 +1000

    feat: add on click placeholder to buttons and organize code

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,7 @@
+import { createButton, processOperation } from "./utils.js";
+
+let buttonClicked = null;
+
 const root = document.querySelector("#root");
 
 const calculator = document.createElement("div");
@@ -14,131 +18,79 @@ content.appendChild(display);
 
 const displayText = document.createElement("span");
 displayText.classList.add("content__display__text");
-displayText.textContent = "100,000,000";
+displayText.textContent = "0";
 display.appendChild(displayText);
 
 const buttonsContainer = document.createElement("div");
 buttonsContainer.classList.add("content__buttons");
 content.appendChild(buttonsContainer);
 
-
 // FIRST ROW
 const firstRow = document.createElement("div");
 firstRow.classList.add("content__buttons__row");
 
-const acBtn = document.createElement("button");
-acBtn.classList.add("btn");
-acBtn.textContent = "AC";
+const acBtn = createButton("AC", ["btn", "btn-operation"]);
+const delBtn = createButton("DEL", ["btn", "btn-operation"]);
+const percentBtn = createButton("%", ["btn", "btn-operation"]);
+const divideBtn = createButton("/", ["btn", "btn-operation"]);
+
 firstRow.appendChild(acBtn);
-
-const plusMinusBtn = document.createElement("button");
-plusMinusBtn.classList.add("btn");
-plusMinusBtn.textContent = "+/-"
-firstRow.appendChild(plusMinusBtn);
-
-const percentBtn = document.createElement("button");
-percentBtn.classList.add("btn");
-percentBtn.textContent = "%";
+firstRow.appendChild(delBtn);
 firstRow.appendChild(percentBtn);
-
-const divideBtn = document.createElement("button");
-divideBtn.classList.add("btn");
-divideBtn.textContent = "/";
 firstRow.appendChild(divideBtn);
-
 
 // SECOND ROW
 const secondRow = document.createElement("div");
 secondRow.classList.add("content__buttons__row");
 
-const sevenBtn = document.createElement("button");
-sevenBtn.classList.add("btn");
-sevenBtn.textContent = "7";
+const sevenBtn = createButton("7", "btn");
+const eightBtn = createButton("8", "btn");
+const nineBtn = createButton("9", "btn");
+const timesBtn = createButton("X", ["btn", "btn-operation"]);
+
 secondRow.appendChild(sevenBtn);
-
-const eightBtn = document.createElement("button");
-eightBtn.classList.add("btn");
-eightBtn.textContent = "8";
 secondRow.appendChild(eightBtn);
-
-const nineBtn = document.createElement("button");
-nineBtn.classList.add("btn");
-nineBtn.textContent = "9";
 secondRow.appendChild(nineBtn);
-
-const timesBtn = document.createElement("button");
-timesBtn.classList.add("btn");
-timesBtn.textContent = "X";
 secondRow.appendChild(timesBtn);
 
 // THIRD ROW
 const thirdRow = document.createElement("div");
 thirdRow.classList.add("content__buttons__row");
 
-const fourBtn = document.createElement("button");
-fourBtn.classList.add("btn");
-fourBtn.textContent = "4";
+const fourBtn = createButton("4", "btn");
+const fiveBtn = createButton("5", "btn");
+const sixBtn = createButton("6", "btn");
+const minusBtn = createButton("-", ["btn", "btn-operation"]);
+
 thirdRow.appendChild(fourBtn);
-
-const fiveBtn = document.createElement("button");
-fiveBtn.classList.add("btn");
-fiveBtn.textContent = "5";
 thirdRow.appendChild(fiveBtn);
-
-const sixBtn = document.createElement("button");
-sixBtn.classList.add("btn");
-sixBtn.textContent = "6";
 thirdRow.appendChild(sixBtn);
-
-const minusBtn = document.createElement("button");
-minusBtn.classList.add("btn");
-minusBtn.textContent = "-";
 thirdRow.appendChild(minusBtn);
-
 
 // FOURTH ROW
 const fourthRow = document.createElement("div");
 fourthRow.classList.add("content__buttons__row");
 
-const oneBtn = document.createElement("button");
-oneBtn.classList.add("btn");
-oneBtn.textContent = "1";
+const oneBtn = createButton("1", "btn");
+const twoBtn = createButton("2", "btn");
+const threeBtn = createButton("3", "btn");
+const plusBtn = createButton("+", ["btn", "btn-operation"]);
+
 fourthRow.appendChild(oneBtn);
-
-const twoBtn = document.createElement("button");
-twoBtn.classList.add("btn");
-twoBtn.textContent = "2";
 fourthRow.appendChild(twoBtn);
-
-const threeBtn = document.createElement("button");
-threeBtn.classList.add("btn");
-threeBtn.textContent = "3";
 fourthRow.appendChild(threeBtn);
-
-const plusBtn = document.createElement("button");
-plusBtn.classList.add("btn");
-plusBtn.textContent = "+";
 fourthRow.appendChild(plusBtn);
-
 
 // FITH ROW
 const fifthRow = document.createElement("div");
 fifthRow.classList.add("content__buttons__row");
 
-const zeroBtn = document.createElement("button");
-zeroBtn.classList.add("btn");
-zeroBtn.textContent = "0";
+const zeroBtn = createButton("0", "btn");
+const dotBtn = createButton(".", "btn");
+const equalsBtn = createButton("=", ["btn", "btn-operation", "btn-equals"]);
+
 fifthRow.appendChild(zeroBtn);
-
-const dotBtn = document.createElement("button");
-dotBtn.classList.add("btn");
-dotBtn.textContent = ".";
 fifthRow.appendChild(dotBtn);
-
-const equalsBtn = document.createElement("button");
-equalsBtn.classList.add("btn");
-equalsBtn.classList.add("btn-equals");
-equalsBtn.textContent = "=";
 fifthRow.appendChild(equalsBtn);
 
 buttonsContainer.appendChild(firstRow);
@@ -146,3 +98,21 @@ buttonsContainer.appendChild(secondRow);
 buttonsContainer.appendChild(thirdRow);
 buttonsContainer.appendChild(fourthRow);
 buttonsContainer.appendChild(fifthRow);
+
+function handleClick() {
+  const buttons = document.querySelectorAll(".btn");
+
+  buttons.forEach(b => {
+    b.addEventListener("click", (e) => {
+      buttonClicked = e.target;
+      const isOperationButton = buttonClicked.classList.contains("btn-operation");
+  
+      if (isOperationButton) {
+        const operation = buttonClicked.textContent.toLowerCase();
+        processOperation(operation);
+      }
+    });
+  });
+}
+
+handleClick();
---
+++ src/js/utils.js
@@ -0,0 +1,46 @@
+export function createButton(text, classes) {
+  const button = document.createElement("button");
+  
+  if (classes) {
+    if (Array.isArray(classes)) {
+      button.classList.add(...classes);
+    } else {
+      button.classList.add(classes);
+    }
+  }
+
+  button.textContent = text;
+  return button;
+}
+
+export function processOperation(operation) {
+  switch (operation) {
+    case "ac":
+      console.log("clear");
+      break;
+    case "del":
+      console.log("delete");
+      break;
+    case "%":
+      console.log("percentage");
+      break;
+    case "/":
+      console.log("divide");
+      break;
+    case "x":
+      console.log("multiply");
+      break;
+    case "-":
+      console.log("subtract");
+      break;
+    case "+":
+      console.log("add");
+      break;
+    case "=":
+      console.log("equals");
+      break;
+    default:
+      console.log("Invalid operation!");
+      break;
+  }
+}
