0e4e110e868a1c59732a7b1d0b3cb9007cf95d48
Victor Zottmann <zottmann.victor@gmail.com>
2025-08-30 01:19:42 +1000

    add operations Map

--- src/js/index.js
+++ src/js/index.js
@@ -15,11 +15,24 @@ const operations = {
   divide: (a, b) => Number(a) / Number(b),
 };
 
+const operationsMap = new Map([
+  ["+", "add"],
+  ["-", "subtract"],
+  ["*", "multiply"],
+  ["÷", "divide"],
+]);
+
 const operate = (op, num1, num2) => {
   const operation = operations[op];
   return operation(num1, num2);
 };
 
 buttons.forEach((b) => {
-  b.addEventListener("click", () => {});
+  b.addEventListener("click", (e) => {
+    const value = e.target.textContent;
+
+    if (operationsMap.has(value)) {
+      state.operator = operationsMap.get(value);
+    }
+  });
 });
