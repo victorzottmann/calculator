2c31d2e5e6c59e6f3bc76177767897aa2fa804a4
Victor Zottmann <zottmann.victor@gmail.com>
2025-08-30 01:43:56 +1000

    calculator is working

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 const buttons = document.querySelectorAll(".btn");
+const displayText = document.querySelector(".content__display__text");
 
-const state = {
+let state = {
   num1: "",
   num2: "",
   operator: "",
@@ -9,10 +10,10 @@ const state = {
 };
 
 const operations = {
-  add: (a, b) => Number(a) + Number(b),
-  subtract: (a, b) => Number(a) - Number(b),
-  multiply: (a, b) => Number(a) * Number(b),
-  divide: (a, b) => Number(a) / Number(b),
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => (a === 0 || b === 0 ? 0 : a / b),
 };
 
 const operationsMap = new Map([
@@ -27,12 +28,65 @@ const operate = (op, num1, num2) => {
   return operation(num1, num2);
 };
 
+function clearState() {
+  state = {
+    num1: "",
+    num2: "",
+    operator: "",
+    operatorClicked: false,
+    result: "",
+  };
+  displayText.textContent = "0";
+}
+
+function showResult() {
+  if (state.num1 && state.num2 && state.operator) {
+    state.result = operate(
+      state.operator,
+      Number(state.num1),
+      Number(state.num2)
+    ).toString();
+    displayText.textContent = state.result;
+  }
+}
+
 buttons.forEach((b) => {
   b.addEventListener("click", (e) => {
-    const value = e.target.textContent;
+    let value = e.target.textContent;
+
+    if (state.result && value !== "AC") {
+      return;
+    }
+
+    if (value === "AC") {
+      clearState();
+      return;
+    }
+
+    if (value === "=") {
+      showResult();
+      return;
+    }
 
     if (operationsMap.has(value)) {
       state.operator = operationsMap.get(value);
+      state.operatorClicked = true;
+      displayText.textContent += ` ${value} `;
+      value = "";
     }
+
+    if (!state.operatorClicked) {
+      state.num1 += value;
+    } else {
+      state.num2 += value;
+    }
+
+    if (displayText.textContent === "0") {
+      displayText.textContent = value;
+    } else {
+      displayText.textContent += value;
+    }
+
+    console.log(state);
   });
 });
