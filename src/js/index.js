64794c7483896c20025173f563ba25f8657914f3
Victor Zottmann <zottmann.victor@gmail.com>
2025-08-30 01:11:00 +1000

    add operations and operate function

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,25 @@
+const buttons = document.querySelectorAll(".btn");
 
+const state = {
+  num1: "",
+  num2: "",
+  operator: "",
+  operatorClicked: false,
+  result: "",
+};
+
+const operations = {
+  add: (a, b) => Number(a) + Number(b),
+  subtract: (a, b) => Number(a) - Number(b),
+  multiply: (a, b) => Number(a) * Number(b),
+  divide: (a, b) => Number(a) / Number(b),
+};
+
+const operate = (op, num1, num2) => {
+  const operation = operations[op];
+  return operation(num1, num2);
+};
+
+buttons.forEach((b) => {
+  b.addEventListener("click", () => {});
+});
